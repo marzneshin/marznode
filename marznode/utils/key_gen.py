3e44999d2518c491d785954fec6d8c16b55b85ca
m0x61h0x64i <109550653+m0x61h0x64i@users.noreply.github.com>
2024-08-12 15:48:32 +0330

    feat(utils): add support reversible key

--- marznode/utils/key_gen.py
+++ marznode/utils/key_gen.py
@@ -4,6 +4,7 @@ import uuid
 
 import xxhash
 
+import binascii
 
 def generate_uuid(key: str) -> uuid.UUID:
     """
@@ -21,3 +22,39 @@ def generate_password(key: str) -> str:
     :return: a 32 characters long hex string
     """
     return xxhash.xxh128(key.encode()).hexdigest()
+
+def generate_uuid_v2(key: str) -> str:
+    """
+    generates a reversible UUID-like string based on key as seed
+    :param key: the seed
+    :return: the uuid
+    """
+    hex_bytes = binascii.unhexlify(key)
+    return str(uuid.UUID(key))
+
+def generate_password_v2(key: str) -> str:
+    """
+    generates a reversible password based on the key as seed
+    :param key: the seed
+    :return: the key
+    """
+    return key
+
+"""Used to convert uuid/password back to a key"""
+
+def generate_key_from_uuid(uuid_string: str) -> str:
+    """
+    converts a UUID-like string back to a key
+    :param uuid_string: the uuid
+    :return: the key
+    """
+    uuid_obj = uuid.UUID(uuid_string)
+    return binascii.hexlify(uuid_obj.bytes).decode('utf-8')
+
+def generate_key_from_password(password: str) -> str:
+    """
+    converts a password back to a key
+    :param password: the seed
+    :return: the password
+    """
+    return password
