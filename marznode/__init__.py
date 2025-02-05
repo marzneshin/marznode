f89400b7d982165f3eaa31e47be6824f9281656e
Dawsh <dawsh@duck.com>
2025-02-05 16:43:26 +0330

    improve(logging): include name in log (#26)

--- marznode/__init__.py
+++ marznode/__init__.py
@@ -7,6 +7,6 @@ from marznode import config
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG if config.DEBUG else logging.INFO)
 handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s: %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
